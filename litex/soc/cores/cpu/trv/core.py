cc5b87c0849d1ad86168db87d16bfd18fb763416
David A Roberts <d@vidr.cc>
2024-03-31 19:56:28 +1000

    trv: instruction decoder

--- litex/soc/cores/cpu/trv/core.py
+++ litex/soc/cores/cpu/trv/core.py
@@ -79,6 +79,53 @@ class TRV(CPU):
         pc = Signal(4)
         self.comb += pc.eq(counter[22:26])
 
+        # reg [31:0] instr;
+        # ...
+        # wire isALUreg  =  (instr[6:0] == 7'b0110011); // rd <- rs1 OP rs2
+        # wire isALUimm  =  (instr[6:0] == 7'b0010011); // rd <- rs1 OP Iimm
+        # wire isBranch  =  (instr[6:0] == 7'b1100011); // if(rs1 OP rs2) PC<-PC+Bimm
+        # wire isJALR    =  (instr[6:0] == 7'b1100111); // rd <- PC+4; PC<-rs1+Iimm
+        # wire isJAL     =  (instr[6:0] == 7'b1101111); // rd <- PC+4; PC<-PC+Jimm
+        # wire isAUIPC   =  (instr[6:0] == 7'b0010111); // rd <- PC + Uimm
+        # wire isLUI     =  (instr[6:0] == 7'b0110111); // rd <- Uimm
+        # wire isLoad    =  (instr[6:0] == 7'b0000011); // rd <- mem[rs1+Iimm]
+        # wire isStore   =  (instr[6:0] == 7'b0100011); // mem[rs1+Simm] <- rs2
+        # wire isSYSTEM  =  (instr[6:0] == 7'b1110011); // special
+        instr = Signal(32)
+        isALUreg = instr[:7] == 0b0110011  # rd <- rs1 OP rs2
+        isALUimm = instr[:7] == 0b0010011  # rd <- rs1 OP Iimm
+        isBranch = instr[:7] == 0b1100011  # if(rs1 OP rs2) PC<-PC+Bimm
+        isJALR = instr[:7] == 0b1100111  # rd <- PC+4; PC<-rs1+Iimm
+        isJAL = instr[:7] == 0b1101111  # rd <- PC+4; PC<-PC+Jimm
+        isAUIPC = instr[:7] == 0b0010111  # rd <- PC + Uimm
+        isLUI = instr[:7] == 0b0110111  # rd <- Uimm
+        isLoad = instr[:7] == 0b0000011  # rd <- mem[rs1+Iimm]
+        isStore = instr[:7] == 0b0100011  # mem[rs1+Simm] <- rs2
+        isSYSTEM = instr[:7] == 0b1110011  # special
+
+        # wire [4:0] rs1Id = instr[19:15];
+        # wire [4:0] rs2Id = instr[24:20];
+        # wire [4:0] rdId  = instr[11:7];
+        # wire [2:0] funct3 = instr[14:12];
+        # wire [6:0] funct7 = instr[31:25];
+        rdId = instr[7:12]
+        funct3 = instr[12:15]
+        rs1Id = instr[15:20]
+        rs2Id = instr[20:25]
+        funct7 = instr[25:]
+
+        # wire [31:0] Uimm={    instr[31],   instr[30:12], {12{1'b0}}};
+        # wire [31:0] Iimm={{21{instr[31]}}, instr[30:20]};
+        # wire [31:0] Simm={{21{instr[31]}}, instr[30:25],instr[11:7]};
+        # wire [31:0] Bimm={{20{instr[31]}}, instr[7],instr[30:25],instr[11:8],1'b0};
+        # wire [31:0] Jimm={{12{instr[31]}}, instr[19:12],instr[20],instr[30:21],1'b0};
+        sign = Replicate(instr[31], 20)
+        Uimm = instr & 0xFFFFF000
+        Iimm = Cat(rs2Id, funct7, sign)
+        Simm = Cat(rdId, funct7, sign)
+        Bimm = Cat(rdId & 0b11110, funct7[:6], rdId[0], sign)
+        Jimm = Cat(rs2Id & 0b11110, funct7[:6], rs2Id[0], sign)
+
         latch = Signal()
         write = 1
         read = 0
